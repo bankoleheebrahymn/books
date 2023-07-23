7dea8e7b6bce649a07bb746aa92f72ae38d1f948
heebrahymne <bankoleheebrahymn@gmail.com>
2023-07-23 17:59:07 +0100

    Deployed E-Book app

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useEffect, useContext } from 'react';
+import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
+import BooksContext from './context/books';
+
+
+
+function App () {
+
+   const {fetchBooks} = useContext(BooksContext);
+    
+useEffect(() => {
+  fetchBooks();
+  }, [fetchBooks]);
+
+
+        return <div  className="App">
+            <h1>Reading List</h1>
+            <BookList />
+        <BookCreate />
+    
+            </div>;
+    
+
+}
+    
+export default App;
